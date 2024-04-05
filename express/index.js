1a2f2830361f3255d198921547759bc0001dcb24
minuwu <84508269+minuwu@users.noreply.github.com>
2024-04-05 23:12:18 +0600

    aall

--- express/index.js
+++ express/index.js
@@ -1,13 +1,43 @@
 import express from "express";
+// import * as path from "path";
 const app = express();
 const port = 3000;
 
 import * as data from "./data.js";
-console.dir(data);
+import { error } from "console";
+// console.dir(data);
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+console.log(__dirname);
+console.log(__filename);
 
 app.set("view engine", "ejs");
+// app.set("view", path.join(__dirname, "./views"));
 app.get("/", (req,res)=>{
     res.render("home");
+})
+
+const drinks = data.data.drinks;
+// console.log();
+// for( let i in drinks){
+//     console.log(drinks[i]);
+// }
+// console.log(drinks);
+
+// let arr = [2,4,5,6,7,8,8,8,1];
+// console.log(arr);
+
+app.get("/drinks", (req,res)=>{
+    if(error){
+        console.log(error);
+    }
+    res.render("instagram", {drinks} );
 })
 
 app.get("/",(req,res)=>{
