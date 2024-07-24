550367c60bbad80bc06ac7bb461c901311e1ca9e
minuwu <84508269+minuwu@users.noreply.github.com>
2024-07-24 21:18:57 +0600

    react form handling #single source of truth

--- react/thirdreact/src/App.jsx
+++ react/thirdreact/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import Lottery from './Lottery.jsx'
 import { sumIsFifteen, allMatching } from './helper.js'
+import Form from './Form/Form.jsx'
 
-function App() {
-  return (
-    <>
-      {/* <Lottery ticketLength={5} winCondition={sumIsFifteen}/> */}
-      <Lottery ticketLength={3} winCondition={allMatching}/>
-      {/* function passing as props */}
-    </>
-  )
+// function App() {
+//   return (
+//     <>
+//       {/* <Lottery ticketLength={5} winCondition={sumIsFifteen}/> */}
+//       <Lottery ticketLength={3} winCondition={allMatching}/>
+//       {/* function passing as props */}
+//     </>
+//   )
+// }
+
+function App(){
+  return <Form/>
 }
 
 export default App
---
+++ react/thirdreact/src/Form/Form.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+export default function Form (){
+    let formTemplate = {
+        fullName: "",
+        userName: "",
+        password: ""
+    }
+    let [formData, setFormData] =  useState(formTemplate);
+    let handleForm = (event) => {
+        setFormData((currentData)=>{
+            return {...currentData, [event.target.name]:event.target.value}
+        })
+    }
+    let handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(formData);
+        setFormData(formTemplate);
+    }
+    
+    return (<form onSubmit={handleSubmit}>
+        <label htmlFor="fullName">fullName</label>
+        <input type="text" name="fullName" id="fullName" value={formData.fullName} onChange={handleForm} />
+
+        <label htmlFor="userName">userName</label>
+        <input type="text"  name="userName"  id="userName" value={formData.userName}  onChange={handleForm}  />
+
+        <label htmlFor="password">password</label>
+        <input type="password"  name="password" id="password"  value={formData.password} onChange={handleForm}  />
+    </form>)
+}
